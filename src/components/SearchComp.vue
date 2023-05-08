151b0f8c3fc4e57a2abd8e7aded23f89752f5a78
Luis-Mocha <luismocha97@gmail.com>
2023-05-08 16:43:25 +0200

    css SearchComp

--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -17,27 +17,33 @@
 
 <template>
 
-    <div>
-        <select name="selectArchetype" id="selectArchetype" v-model="store.inputArchetype">
-            <option v-for="(elem, index) in store.arrayArchetype" :key="index" :value="elem.archetype_name">
-                {{ elem.archetype_name }}
-            </option>
-        </select>
-        <button type="submit" @click.prevent="$emit('selezionaArchetipo')">
-            Cerca Archetipo
-        </button>
+    <div class="searchDiv">
+        <div>
+            <select name="selectArchetype" id="selectArchetype" v-model="store.inputArchetype">
+                <option v-for="(elem, index) in store.arrayArchetype" :key="index" :value="elem.archetype_name">
+                    {{ elem.archetype_name }}
+                </option>
+            </select>
+            <button type="submit" @click.prevent="$emit('selezionaArchetipo')">
+                Cerca Archetipo
+            </button>
+        </div>
+
+        
+        <div>
+            <input type="search" name="inputName" id="inputName" placeholder="Filter by name" v-model="store.testoRicerca" >
+        </div>
     </div>
 
     
-    <div>
-        <input type="search" name="inputName" id="inputName" placeholder="Filter by name" v-model="store.testoRicerca" @keyup.enter="$emit('filtraNomeEmit')" @keyup.delete="$emit('filtraNomeEmit')"> 
-        <!-- <button type="submit" @click.prevent="$emit('nomeEmit')">
-            Filtra per nome
-        </button> -->
-    </div>
 
 </template>
 
 <style lang="scss" scoped>
-
+    .searchDiv {
+        display: flex;
+        justify-content: space-evenly;
+        padding-top: 15px;
+        font-size: 18px;
+    }
 </style>
