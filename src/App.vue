341a17570c8f3e41cbfcfa47a281574179987296
Luis-Mocha <luismocha97@gmail.com>
2023-05-08 12:03:10 +0200

    input ricerca carta in base al nome

--- src/App.vue
+++ src/App.vue
@@ -4,33 +4,69 @@ import axios from 'axios';
 import {store} from './store'
 import HeaderComp from './components/HeaderComp.vue';
 import PersonaggiComp from './components/PersonaggiComp.vue';
+import SearchComp from './components/SearchComp.vue';
 
 export default {
   name: 'app',
   components: {
     HeaderComp,
     PersonaggiComp,
+    SearchComp,
   },
   data() {
     return {
       store
     }
   },
-  created () {
-    this.callApi()
-  },
-  methods: {
-    callApi() {
-      axios.get( 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=24&offset=0')
-      .then( (res) => {
-        console.log(res.data.data) //array personaggi yu-gi-oh
+  // created () {
+  //   this.callApi()
+  // },
+  computed: {
+    //funzione Call Api
+      callApi() {
+        if (store.testoRicerca !== '') {
+          axios.get( `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${store.testoRicerca}`)
+          .then( (res) => {
+          console.log(res.data.data) //array personaggi yu-gi-oh
         
-        const infoApi = res.data.data;
-        store.arrayPersonaggi = infoApi;
-      })
-    },
-  }
-
+          const infoApi = res.data.data;
+          store.arrayPersonaggi = infoApi;
+        })
+        } else {
+          axios.get( 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=24&offset=0')
+          .then( (res) => {
+            console.log(res.data.data) //array personaggi yu-gi-oh
+          
+            const infoApi = res.data.data;
+            store.arrayPersonaggi = infoApi;
+          })
+        }
+      }
+    // Fine Call Api
+  },
+  // methods: {
+  //   //funzione Call Api
+  //      callApi() {
+  //        if (store.testoRicerca !== '') {
+  //          axios.get( `https:db.ygoprodeck.com/api/v7/cardinfo.php?num=${store.testoRicerca}&offset=0`)
+  //          .then( (res) => {
+  //          console.log(res.data.data) //array personaggi yu-gi-oh
+        
+  //          const infoApi = res.data.data;
+  //          store.arrayPersonaggi = infoApi;
+  //        })
+  //        } else {
+  //          axios.get( 'https:db.ygoprodeck.com/api/v7/cardinfo.php?num=24&offset=0')
+  //          .then( (res) => {
+  //            console.log(res.data.data) //array personaggi yu-gi-oh
+          
+  //            const infoApi = res.data.data;
+  //            store.arrayPersonaggi = infoApi;
+  //          })
+  //        }
+  //      }
+  //   //Fine Call Api
+  // }
 }
 
 </script>
@@ -40,6 +76,8 @@ export default {
   <HeaderComp :titoloProps = " 'Yu-Gi-Oh!' " :classiProps="'text-center text-danger'" />
 
   <main>
+
+    <SearchComp @nomeEmit="callApi()"/>
     
     <PersonaggiComp/>
 
---
+++ src/components/SearchComp.vue
@@ -0,0 +1,31 @@
+<script>
+    import {store} from '../store'
+
+    export default {
+        name: 'SearchComp',
+
+        data() {
+            return {
+                store
+            }
+        },
+        
+    }
+    
+</script>
+
+
+<template>
+
+    <div>
+        <input type="text" placeholder="Search Card" v-model="store.testoRicerca" @keyup.enter.prevent="$emit('nomeEmit')"> <!-- @keyup.prevent="$emit('nomeEmit') -->
+        <button type="submit" @click.prevent="$emit('nomeEmit')">
+            Ricerca per nome
+        </button>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
