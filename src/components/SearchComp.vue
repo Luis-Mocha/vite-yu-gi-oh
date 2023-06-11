a6c2e919bf35643274648492ddfa29e5e4641838
Luis-Mocha <luismocha97@gmail.com>
2023-06-11 22:50:55 +0200

    .

--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -17,20 +17,21 @@
 
 <template>
 
-    <div class="searchDiv">
-        <div>
-            <select name="selectArchetype" id="selectArchetype" v-model="store.inputArchetype">
-                <option v-for="(elem, index) in store.arrayArchetype" :key="index" :value="elem.archetype_name">
-                    {{ elem.archetype_name }}
-                </option>
-            </select>
-            <button type="submit" @click.prevent="$emit('selezionaArchetipo')">
-                Cerca Archetipo
-            </button>
+    <div class="searchDiv bg-light">
+        <div class="mb-3">
+            <div class="d-flex flex-column">
+                <label for="selectArchetype" class="form-label">Seleziona Archetipo</label>
+                <select name="selectArchetype" id="selectArchetype" v-model="store.inputArchetype" @change="$emit('selezionaArchetipo')">
+                    <option v-for="(elem, index) in store.arrayArchetype" :key="index" :value="elem.archetype_name">
+                        {{ elem.archetype_name }}
+                    </option>
+                </select>
+            </div>
+            
         </div>
 
         
-        <div>
+        <div class="mb-3 d-flex align-items-center">
             <input type="search" name="inputName" id="inputName" placeholder="Filter by name" v-model="store.testoRicerca" >
         </div>
     </div>
@@ -42,8 +43,13 @@
 <style lang="scss" scoped>
     .searchDiv {
         display: flex;
+        flex-wrap: wrap;
         justify-content: space-evenly;
         padding-top: 15px;
         font-size: 18px;
+
+        border-width: 8px;
+        border-color: black;
+        border-style: ridge;
     }
 </style>
